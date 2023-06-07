7c2e8440260182056141a54cd7e6b6928b18c1b4
verartg <verarottger@gmail.com>
2023-06-07 09:11:54 +0200

    start applicatie

---
+++ src/main/java/com/example/maatjes/MaatjesApplication.java
@@ -0,0 +1,13 @@
+package com.example.maatjes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaatjesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaatjesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maatjes/MaatjesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maatjes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaatjesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
